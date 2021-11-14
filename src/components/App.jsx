deca79ac293870d3d6d5c005e3682dce6fcab67f
Howie21 <bryce9643@gmail.com>
2021-11-14 11:29:34 -0500

    Add TitleBar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import './App.css';
+import TitleBar from "./TitleBar/TitleBar";
 
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "Ready Player One", author: "Ernest Cline"},
+            {title: "All the Light we Cannot See", author: "Anthony Doerr"},
+            {title: "The First and Last Freedom", author: "Jiddu K."},
+        ]
+        this.state = {
+            bookNumber: 0
+        };
 
     }
 
     render(){
         return (
-            <h1>Our React app from a Component file</h1>
+            <div className="container-fluid">
+                <TitleBar />
+                <div className="row">
+                    <div className="col-m-4">
+                    { /* Button here */}
+                    </div>
+                    <div className="col-m-4">
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-m-4"></div>
+                </div>
+            </div>
         )
     }
 }
