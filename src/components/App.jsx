82bc8264254e4ebc7dce9d03d1147bc4d51836ff
Howie21 <bryce9643@gmail.com>
2021-11-14 11:06:54 -0500

    Completion of Video 3

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import './App.css';
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+
+    }
+
+    render(){
+        return (
+            <h1>Our React app from a Component file</h1>
+        )
+    }
+}
+
+export default App;
