e7ba1964875ff99a4af49d88f7490fb45164fcea
Howie21 <bryce9643@gmail.com>
2021-11-15 11:53:45 -0500

    Added functionality to change the book on the rendered page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,19 +17,32 @@ class App extends Component {
 
     }
 
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        }); 
+    }
+
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
                 <div className="row">
-                    <div className="col-m-4">
+                    <div className="col-md-4">
                     { /* Button here */}
                     </div>
-                    <div className="col-m-4">
+                    <div className="col-md-4">
                         <h1>{this.books[this.state.bookNumber].title}</h1>
                         <h4>{this.books[this.state.bookNumber].author}</h4>
                     </div>
-                    <div className="col-m-4"></div>
+                    <div className="col-md-4">
+                        <button onClick={this.goToNextBook}>Next Book!</button>
+                    </div>
                 </div>
             </div>
         )
